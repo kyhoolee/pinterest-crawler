8d54b14325e70327d1045ab595631fe75a9773c2
kyhoolee <kyhoolee@gmail.com>
2022-07-14 16:19:55 +0700

    Update: get more data

--- main.py
+++ main.py
@@ -2,21 +2,32 @@
 from src import PinterestScraper, PinterestSearchConfig, PinterestRelatedConfig
 
 
-configs = PinterestSearchConfig(search_keywords="character face cyberpunk", # Search word
-                          file_lengths=50,     # total number of images to download (default = "100")
-                          image_quality="orig", # image quality (default = "orig")
-                          bookmarks="")         # next page data (default= "")
+# configs = PinterestSearchConfig(
+#     search_keywords="Goth FACE", # Search word
+#     file_lengths=50,     # total number of images to download (default = "100")
+#     image_quality="orig", # image quality (default = "orig")
+#     bookmarks="")         # next page data (default= "")
+# urls = PinterestScraper(configs).get_search_urls()   # just bring image links
 
-configs = PinterestRelatedConfig(search_keywords="860257966336607477", # pin
-                          file_lengths=50,     # total number of images to download (default = "100")
-                          image_quality="orig", # image quality (default = "orig")
-                          bookmarks="")         # next page data (default= "")                         
+
+
+# 398427898289857670
+
+
+configs = PinterestRelatedConfig(
+    search_keywords="1041527851297924777", # pin
+    file_lengths=50,     # total number of images to download (default = "100")
+    image_quality="orig", # image quality (default = "orig")
+    bookmarks="")         # next page data (default= "")                         
+urls = PinterestScraper(configs).get_related_urls()
 
 
 # PinterestScraper(configs).download_search_images()     # download images directly
-# print(PinterestScraper(configs).get_search_urls())     # just bring image links
+# 
 
+# PinterestScraper(configs).download_related_images()  
 
-PinterestScraper(configs).download_related_images()  
+print('\n\n')
+print(urls)
 
 
